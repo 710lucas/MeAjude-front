3d95e4c506ef11ca4fa849f943b9b2c44ac689d8
Lucas Perônico Barbotin <lucas.peronico@dcx.ufpb.br>
2023-10-26 14:54:34 -0300

    geral

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -2,31 +2,60 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators'
 import { map } from 'rxjs/operators'
+import { UserService } from './user-service.service';
+import { Donation } from './donation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient, private userService : UserService) { }
 
   api_url : string = "http://127.0.0.1:8081";
   httpOptions = {
     headers : new HttpHeaders({'Content-Type' : 'application/json'})
   }
 
-  postAuth(email : string, password: string){
-    this.http.post(this.api_url+"/auth",  {"email" : email, "password" : password},this.httpOptions).subscribe(
-      (response : any) =>{
-        console.log(response)
-        if(response.token)
-          return response.token
-      },
-      (error) =>{
-        console.error(error)
-        return null
-      }
-    )
+
+  setAuthorizationHeader(){
+    this.httpOptions = {
+     headers:  new HttpHeaders({'Authorization' : `${localStorage.getItem("token")}`})
+    }
+  }
+
+  getCampanhaBydId(id: string | null){
+    this.setAuthorizationHeader();
+    return this.http.get(this.api_url+"/campaigns/"+id, this.httpOptions)
+  }
+
+  getCampanhas(){
+    this.setAuthorizationHeader()
+    return this.http.get(this.api_url+"/campaigns", this.httpOptions)
+  }
+
+  getUser(id: number){
+    this.setAuthorizationHeader()
+    return this.http.get(this.api_url+"/users/"+id, this.httpOptions)
+  }
+
+  postDonation(donation : Donation){
+    this.setAuthorizationHeader()
+    return  this.http.post(this.api_url+"/donation", donation, this.httpOptions)
+  }
+
+  async postAuth(email : string, password: string){
+    return this.http.post(this.api_url+"/auth",  {"email" : email, "password" : password},this.httpOptions)
+  }
+
+  getUsers(){
+    this.setAuthorizationHeader()
+    return this.http.get(this.api_url+"/users", this.httpOptions)
+  }
+
+  createUser(){
+    this.setAuthorizationHeader()
+    return this.http.post(this.api_url+"/users", this.httpOptions)
   }
 
 }
